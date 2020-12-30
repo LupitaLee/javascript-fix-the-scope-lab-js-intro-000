598c45b059a33fab072300e98a66c056d589f1fe
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-30 16:49:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function myAnimal() {
   return animal
 }
 
-function yourAnimal() {var animal();
+function yourAnimal() {
   // The tests expect this function to return `animal` just like the previous function
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope
   // How can we make sure that this function
@@ -13,7 +13,6 @@ function yourAnimal() {var animal();
   // P.S.: Hard-coding 'cat' below will not work
   return animal
 }
-yourAnimal()
 
 
 function add2(n) {
